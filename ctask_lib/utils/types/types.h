eb1adb90901ebade5e20db8d073f5b7266048a05
Aliaksandr Shcherba <salsarodrigues@icloud.com>
2025-03-19 13:08:10 +0300

    feat: CliParser unit tests

---
+++ ctask_lib/utils/types/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <string>
+
+namespace ctask::utils::types
+{
+    /**
+    * @struct CliArgs
+    * @brief Structure for storing command-line arguments.
+    *
+    * This structure is used to store application configurations.
+    */
+    struct CliArgs
+    {
+    };
+}
+
+#endif //TYPES_H
